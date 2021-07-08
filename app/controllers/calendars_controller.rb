9214a82dc9a886f123ff5cdfef0afdf9b8b8c970
Chawangmushi <satoshi0112@gmail.com>
2021-07-08 17:17:43 +0900

    いつの間にか消えてしまったwday = Date.today.wdayを追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,12 +20,14 @@ class CalendarsController < ApplicationController
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+  
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
+    wday = Date.today.wday
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
@@ -36,6 +38,7 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = wday + x
+
       if wday_num >= 7
         wday_num = wday_num -7
       end
